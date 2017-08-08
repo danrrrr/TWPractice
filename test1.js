923964f3cec1dda5a5f79407869d8b2c1b86d075
danr <851812859@qq.com>
2017-08-08 14:20:30 +0800

    task1

---
+++ test1.js
@@ -0,0 +1,14 @@
+function getPrime(num){
+    let i = 2;
+    let result = '';
+    while(i <= num){
+        if(num % i === 0){
+            result += i;
+            num /= i;
+        }else{
+            i++;
+        }
+    }
+    return result;
+}
+console.log(getPrime(90));
